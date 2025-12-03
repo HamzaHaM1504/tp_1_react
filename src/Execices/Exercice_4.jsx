bb0b9a84280d37f951e7bcd4ac8cf7b8d83d1163
hamza_hammouch <hammouc1023@gmail.com>
2025-12-03 16:05:59 +0100

    2nd commit

--- src/Execices/Exercice_4.jsx
+++ src/Execices/Exercice_4.jsx
@@ -1,32 +1,16 @@
 import { useState } from "react";
-import deImg from "./images/de.jpg";
-import i1 from "./images/1.jpg";
-import i2 from "./images/2.jpg";
-import i3 from "./images/3.jpg";
-import i4 from "./images/4.jpg";
-import i5 from "./images/5.jpg";
-import i6 from "./images/6.jpg";
 
 export default function Cube() {
     const faceCache = 5
-    const images = {
-        de: deImg,
-        1: i1,
-        2: i2,
-        3: i3,
-        4: i4,
-        5: i5,
-        6: i6,
-    };
     const [face ,setFace] = useState(null)
     const [essais ,setEssais] = useState(0)
     const [message ,setMessage] = useState('')
-    const [photo , setPhoto] = useState(images.de)
+    const [photo , setPhoto] = useState("/de.jpg")
     const jouer = () => {
         const valeur = Math.floor(Math.random() * 6) + 1
         setFace(valeur)
         setEssais(essais + 1)
-        setPhoto(images[valeur])
+        setPhoto(`/${valeur}.jpg`)
 
         if (valeur === faceCache) {
             setMessage("Bravo vous avez trouvez la face cachee!")
